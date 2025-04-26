8763ed640c4444b2b4ddd6282606677847ed3ed2
Aaron <200678797+OtterLover6000@users.noreply.github.com>
2025-04-25 23:51:45 -0700

    Complete assignment

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include "main.hpp"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
 
 int main()
 {
@@ -11,5 +16,5 @@ int main()
     cout << "File created with " << empN << " employee records\n";
 
     empN = readFile(filename);
-    cout << "The total number of employess read " << empN << endl;
+    cout << "The total number of employees read " << empN << endl;
 }
--- main.hpp
+++ main.hpp
@@ -1,3 +1,46 @@
 /***************************************************
- * Code your program here
+* Code your program here
  ***************************************************/
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+inline int writeFile(const string &fileName) {
+    int employeeCount;
+    cin >> employeeCount;
+
+    ofstream outFile(fileName);
+
+    for (int i = 0; i < employeeCount; i++) {
+        int id;
+        string name;
+        string department;
+        int salary;
+        cin >> id >> name >> department >> salary;
+        outFile << id << " " << name << " " << department << " " << salary << endl;
+    }
+    outFile.close();
+
+
+    return employeeCount;
+}
+
+inline int readFile(const string &fileName) {
+    ifstream inFile(fileName);
+
+    int id;
+    int salary;
+    string name;
+    string department;
+
+    int count = 0;
+    while (inFile >> id >> name >> department >> salary) {
+        cout << id << " " << name << " " << department << " " << salary << endl;
+        count++;
+    }
+    inFile.close();
+    return count;
+}
